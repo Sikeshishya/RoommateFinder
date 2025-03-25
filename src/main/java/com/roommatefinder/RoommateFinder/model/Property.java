64d0f17494c7f0657f05a8359c3c359e6824f20f
Sikeshishya <1157966943+Sikeshishya@users.noreply.github.com>
2025-03-25 23:58:06 +0530

    updated pom.xml for JWT

--- src/main/java/com/roommatefinder/RoommateFinder/model/Property.java
+++ src/main/java/com/roommatefinder/RoommateFinder/model/Property.java
@@ -1,11 +1,9 @@
 package com.roommatefinder.RoommateFinder.model;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "properties")
-@Data
 public class Property {
 
     @Id
@@ -23,6 +21,55 @@ public class Property {
         this.description = description;
         this.location = location;
         this.budget = budget;
+        this.preferredGender = preferredGender;
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public String getPreferredGender() {
+        return preferredGender;
+    }
+
+    public void setPreferredGender(String preferredGender) {
         this.preferredGender = preferredGender;
     }
 }
